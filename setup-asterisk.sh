de570974655e328e13fb49d26e1022cb22cc376c
Charlie Root <root@kamailio1.my.domain>
2026-06-25 23:31:19 +0300

    wip

--- setup-asterisk.sh
+++ setup-asterisk.sh
@@ -2,7 +2,18 @@
 pkg update -f
 pkg install -y asterisk22
 
-add/overlay server-conf/ into /usr/local/etc/asterisk/
+if [ ! -d /usr/local/etc/asterisk ]; then
+	echo "no such /usr/local/etc/asterisk/"
+	exit 1
+fi
 
 service asterisk enable
+
+cat <<EOF
+ ADD CONF/OVERLAY per host:
+
+cp asterisk-IP/ into /usr/local/etc/asterisk/
+
+EOF
+
 service asterisk start
